d24544d22669c627586153f38e74c37405393781
sunilnikam@hotmail.com <sunilnikam@hotmail.com>
2025-11-20 16:20:45 -0500

    Add project files.

---
+++ Concept/DelegateTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestPOC.Concept
+{
+    delegate void Calculator(int x, int y);
+    public static class DelegateTest
+    {
+        public static void TestRegular()
+        {
+            Calculator calculator = new Calculator(DelegateTest.Add);
+            calculator(10, 20);
+
+            calculator = new Calculator(DelegateTest.Subtract);
+            calculator(30, 15);
+
+            Console.WriteLine("Press any key to exit...");
+        }
+
+        public static void TestMulticast()
+        {
+            Calculator calculator = new Calculator(DelegateTest.Add);
+            calculator += DelegateTest.Subtract;
+            calculator(50, 25);
+            Console.WriteLine("Press any key to exit...");
+        }
+
+        public static void TestAnonymousMethod()
+        {
+            Calculator calculator = delegate (int x, int y)
+            {
+                Console.WriteLine($"Product: {x * y}");
+            };
+            calculator(5, 6);
+            Console.WriteLine("Press any key to exit...");
+        }
+        
+        public static void TestLambdaExpression()
+        {
+            Calculator calculator = (x, y) =>
+            {
+                Console.WriteLine($"Quotient: {x / y}");
+            };
+            calculator(20, 4);
+            Console.WriteLine("Press any key to exit...");
+        }       
+
+        public static void Add(int a, int b)
+        {
+            Console.WriteLine($"Sum: {a + b}");
+        }
+
+        public static void Subtract(int a, int b)
+        {
+            Console.WriteLine($"Difference: {a - b}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using TestPOC.Concept;
+
+namespace TestPOC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //DelegateTest.TestRegular();
+            DelegateTest.TestMulticast();
+
+        }        
+    }
+}
